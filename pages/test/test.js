7266010fc62573ee17b628e4a740c265da617c02
tomfriwel <976932447@qq.com>
2019-04-10 17:50:01 +0800

    test background image

---
+++ pages/test/test.js
@@ -0,0 +1,124 @@
+// pages/test/test.js
+const QRCode = require('./weapp-qrcode-test.js')
+import rpx2px from '../../utils/rpx2px.js'
+let qrcode;
+
+// 300rpx 在6s上为 150px
+const qrcodeWidth = rpx2px(600)
+let src = '/images/test-w.png'
+
+Page({
+    data: {
+        text: 'https://github.com/tomfriwel/weapp-qrcode',
+        image: '',
+        // 用于设置wxml里canvas的width和height样式
+        qrcodeWidth: qrcodeWidth,
+        imgsrc: ''
+    },
+    onLoad: function(options) {
+    },
+    onReady() {
+        const z = this
+        qrcode = new QRCode('canvas', {
+            usingIn: this, // usingIn 如果放到组件里使用需要加这个参数
+            // text: "https://github.com/tomfriwel/weapp-qrcode",
+            backgroundImage: src,
+            logoImage: '/images/test.jpg',
+            width: qrcodeWidth,
+            height: qrcodeWidth,
+            size: qrcodeWidth,
+            colorDark: "#DA4967",
+            colorLight: "white",
+            margin:20,
+            correctLevel: QRCode.CorrectLevel.H,
+        });
+
+        // 生成图片，绘制完成后调用回调
+        qrcode.makeCode(z.data.text, () => {
+            // 回调
+            qrcode.exportImage(function(path) {
+                z.setData({
+                    imgsrc: path
+                })
+            })
+        })
+    },
+    test() {
+        const canvasId = 'temp'
+        let ctx = wx.createCanvasContext(canvasId, this)
+        ctx.drawImage(src, 0, 0, qrcodeWidth, qrcodeWidth)
+        ctx.draw(false, () => {
+            wx.canvasGetImageData({
+                canvasId: canvasId,
+                x: 0,
+                y: 0,
+                width: qrcodeWidth,
+                height: qrcodeWidth,
+                success(res) {
+                    console.log(res)
+                    // console.log(qrcode.getAverageRGB(res))
+                    wx.getImageInfo({
+                        src,
+                        success: function(imageInfo) {
+                            qrcode.testMake(res, imageInfo)
+                        },
+                    })
+                },
+            })
+        })
+        wx.getImageInfo({
+            src: '/images/test.jpg',
+            success: res => {
+                console.log(res)
+            }
+        })
+    },
+    confirmHandler: function(e) {
+        let {
+            value
+        } = e.detail
+        this.renderCode(value)
+    },
+    renderCode(value) {
+        const z = this
+        console.log('make handler')
+        qrcode.makeCode(value, () => {
+            console.log('make')
+            qrcode.exportImage(function(path) {
+                console.log(path)
+                z.setData({
+                    imgsrc: path
+                })
+            })
+        })
+    },
+    inputHandler: function(e) {
+        var value = e.detail.value
+        this.setData({
+            text: value
+        })
+    },
+    tapHandler: function() {
+        this.renderCode(this.data.text)
+    },
+    // 长按保存
+    save: function() {
+        console.log('save')
+        wx.showActionSheet({
+            itemList: ['保存图片'],
+            success: function(res) {
+                console.log(res.tapIndex)
+                if (res.tapIndex == 0) {
+                    qrcode.exportImage(function(path) {
+                        wx.saveImageToPhotosAlbum({
+                            filePath: path,
+                            complete:res=>{
+                                console.log(res)
+                            }
+                        })
+                    })
+                }
+            }
+        })
+    }
+})
