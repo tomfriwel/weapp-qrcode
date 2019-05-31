319df4b02a168231d74feea79b277266927be2cc
tomfriwel <976932447@qq.com>
2019-05-31 13:44:59 +0800

    解决二维码乱码的问题

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,75 @@
 // pages/index/index.js
 var QRCode = require('../../utils/weapp-qrcode.js')
+import rpx2px from '../../utils/rpx2px.js'
+
 var qrcode;
 
+// 300rpx 在6s上为 150px
+const qrcodeWidth = rpx2px(300)
 
 Page({
-    data: {
-        text: 'https://github.com/tomfriwel/weapp-qrcode',
-        image: ''
-    },
-    onLoad: function (options) {
-        qrcode = new QRCode('canvas', {
-            // usingIn: this,
-            text: "https://github.com/tomfriwel/weapp-qrcode",
-            image:'/images/bg.jpg',
-            width: 150,
-            height: 150,
-            colorDark: "#1CA4FC",
-            colorLight: "white",
-            correctLevel: QRCode.CorrectLevel.H,
-        });
-    },
-    confirmHandler: function (e) {
-        var value = e.detail.value
-        qrcode.makeCode(value)
-    },
-    inputHandler: function (e) {
-        var value = e.detail.value
-        this.setData({
-            text: value
-        })
-    },
-    tapHandler: function () {
-        // 传入字符串生成qrcode
-        qrcode.makeCode(this.data.text)
-    },
-    // 长按保存
-    save: function () {
-        console.log('save')
-        wx.showActionSheet({
-            itemList: ['保存图片'],
-            success: function (res) {
-                console.log(res.tapIndex)
-                if (res.tapIndex == 0) {
-                    qrcode.exportImage(function (path) {
-                        wx.saveImageToPhotosAlbum({
-                            filePath: path,
-                        })
-                    })
-                }
-            }
-        })
-    }
+  data: {
+    text: 'https://github.com/tomfriwel/weapp-qrcode',
+    qrcodeWidth: qrcodeWidth,
+    image: '',
+  },
+  onLoad: function(options) {
+  },
+  onReady() {
+    const z = this
+    qrcode = new QRCode('canvas', {
+      usingIn: this,
+      // text: "https://github.com/tomfriwel/weapp-qrcode",
+      image: '/images/bg.jpg',
+      width: qrcodeWidth,
+      height: qrcodeWidth,
+      // width: 150,
+      // height: 150,
+      colorDark: "#1CA4FC",
+      colorLight: "white",
+      correctLevel: QRCode.CorrectLevel.H,
+    });
+
+    // 生成图片，绘制完成后调用回调
+    qrcode.makeCode(z.data.text)
+  },
+  confirmHandler: function(e) {
+    var value = e.detail.value
+    qrcode.makeCode(value)
+  },
+  inputHandler: function(e) {
+    var value = e.detail.value
+    this.setData({
+      text: value
+    })
+  },
+  generateImage: function(e) {
+    const z = this
+    qrcode.exportImage(function (path) {
+      z.setData({
+        imgsrc: path
+      })
+    })
+  },
+  tapHandler: function() {
+    // 传入字符串生成qrcode
+    qrcode.makeCode(this.data.text)
+  },
+  // 长按保存
+  save: function() {
+    console.log('save')
+    wx.showActionSheet({
+      itemList: ['保存图片'],
+      success: function(res) {
+        console.log(res.tapIndex)
+        if (res.tapIndex == 0) {
+          qrcode.exportImage(function(path) {
+            wx.saveImageToPhotosAlbum({
+              filePath: path,
+            })
+          })
+        }
+      }
+    })
+  }
 })
--- pages/responsive/responsive.js
+++ pages/responsive/responsive.js
@@ -7,81 +7,82 @@ let qrcode;
 const qrcodeWidth = rpx2px(300)
 
 Page({
-    data: {
-        text: 'https://github.com/tomfriwel/weapp-qrcode',
-        image: '',
-        // 用于设置wxml里canvas的width和height样式
-        qrcodeWidth: qrcodeWidth,
-        imgsrc: ''
-    },
-    onLoad: function(options) {
-    },
-    onReady() {
-        const z = this
-        qrcode = new QRCode('canvas', {
-            usingIn: this, // usingIn 如果放到组件里使用需要加这个参数
-            // text: "https://github.com/tomfriwel/weapp-qrcode",
-            image: '/images/bg.jpg',
-            width: qrcodeWidth,
-            height: qrcodeWidth,
-            colorDark: "#DA4967",
-            colorLight: "white",
-            correctLevel: QRCode.CorrectLevel.H,
-        });
+  data: {
+    text: 'https://github.com/tomfriwel/weapp-qrcode',
+    image: '',
+    // 用于设置wxml里canvas的width和height样式
+    qrcodeWidth: qrcodeWidth,
+    imgsrc: ''
+  },
+  onLoad: function(options) {},
+  onReady() {
+    const z = this
+    qrcode = new QRCode('canvas', {
+      usingIn: this, // usingIn 如果放到组件里使用需要加这个参数
+      // text: "https://github.com/tomfriwel/weapp-qrcode",
+      image: '/images/bg.jpg',
+      width: qrcodeWidth,
+      height: qrcodeWidth,
+      colorDark: "#DA4967",
+      colorLight: "white",
+      correctLevel: QRCode.CorrectLevel.H,
+    });
 
-        // 生成图片，绘制完成后调用回调
-        qrcode.makeCode(z.data.text, () => {
-            // 回调
-            qrcode.exportImage(function (path) {
-                z.setData({
-                    imgsrc: path
-                })
+    // 生成图片，绘制完成后调用回调
+    qrcode.makeCode(z.data.text, () => {
+      // 回调
+      setTimeout(() => {
+        qrcode.exportImage(function(path) {
+          z.setData({
+            imgsrc: path
+          })
+        })
+      }, 200)
+    })
+  },
+  confirmHandler: function(e) {
+    let {
+      value
+    } = e.detail
+    this.renderCode(value)
+  },
+  renderCode(value) {
+    const z = this
+    console.log('make handler')
+    qrcode.makeCode(value, () => {
+      console.log('make')
+      qrcode.exportImage(function(path) {
+        console.log(path)
+        z.setData({
+          imgsrc: path
+        })
+      })
+    })
+  },
+  inputHandler: function(e) {
+    var value = e.detail.value
+    this.setData({
+      text: value
+    })
+  },
+  tapHandler: function() {
+    this.renderCode(this.data.text)
+  },
+  // 长按保存
+  save: function() {
+    console.log('save')
+    wx.showActionSheet({
+      itemList: ['保存图片'],
+      success: function(res) {
+        console.log(res.tapIndex)
+        if (res.tapIndex == 0) {
+          qrcode.exportImage(function(path) {
+            wx.saveImageToPhotosAlbum({
+              filePath: path,
             })
-        })
-    },
-    confirmHandler: function(e) {
-        let {
-            value
-        } = e.detail
-        this.renderCode(value)
-    },
-    renderCode(value) {
-        const z = this
-        console.log('make handler')
-        qrcode.makeCode(value, () => {
-            console.log('make')
-            qrcode.exportImage(function (path) {
-                console.log(path)
-                z.setData({
-                    imgsrc: path
-                })
-            })
-        })
-    },
-    inputHandler: function(e) {
-        var value = e.detail.value
-        this.setData({
-            text: value
-        })
-    },
-    tapHandler: function() {
-        this.renderCode(this.data.text)
-    },
-    // 长按保存
-    save: function() {
-        console.log('save')
-        wx.showActionSheet({
-            itemList: ['保存图片'],
-            success: function(res) {
-                console.log(res.tapIndex)
-                if (res.tapIndex == 0) {
-                    qrcode.exportImage(function(path) {
-                        wx.saveImageToPhotosAlbum({
-                            filePath: path,
-                        })
-                    })
-                }
-            }
-        })
-    }
+          })
+        }
+      }
+    })
+  }
 })
